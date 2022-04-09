4241d9a94d5cccac5e16f316a96269b281002647
chris-wjn <chrisnelson315@gmail.com>
2022-04-09 16:00:20 -0700

    started updating to pull values from JSON for entity resistance.

--- src/main/java/com/cwjn/idf/Attributes/AttributeAttachEvent.java
+++ src/main/java/com/cwjn/idf/Attributes/AttributeAttachEvent.java
@@ -5,6 +5,7 @@ import com.cwjn.idf.Config.JSONHandler;
 import com.cwjn.idf.ImprovedDamageFramework;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,18 +18,31 @@ public class AttributeAttachEvent {
         for (EntityType<? extends LivingEntity> entityType : event.getTypes()) {
             //damage
             //TODO: grab values from json to put here!
+            if (entityType == EntityType.PLAYER) {
+                event.add(entityType, AttributeRegistry.FIRE_DAMAGE.get());
+                event.add(entityType, AttributeRegistry.WATER_DAMAGE.get());
+                event.add(entityType, AttributeRegistry.LIGHTNING_DAMAGE.get());
+                event.add(entityType, AttributeRegistry.MAGIC_DAMAGE.get());
+                event.add(entityType, AttributeRegistry.DARK_DAMAGE.get());
+                //resistances
+                event.add(entityType, AttributeRegistry.FIRE_RESISTANCE.get());
+                event.add(entityType, AttributeRegistry.WATER_RESISTANCE.get());
+                event.add(entityType, AttributeRegistry.LIGHTNING_RESISTANCE.get());
+                event.add(entityType, AttributeRegistry.MAGIC_RESISTANCE.get());
+                event.add(entityType, AttributeRegistry.DARK_RESISTANCE.get());
+            }
             EntityData data = JSONHandler.getEntityData(entityType.getRegistryName());
-            event.add(entityType, AttributeRegistry.FIRE_DAMAGE.get());
-            event.add(entityType, AttributeRegistry.WATER_DAMAGE.get());
-            event.add(entityType, AttributeRegistry.LIGHTNING_DAMAGE.get());
-            event.add(entityType, AttributeRegistry.MAGIC_DAMAGE.get());
-            event.add(entityType, AttributeRegistry.DARK_DAMAGE.get());
+            event.add(entityType, AttributeRegistry.FIRE_DAMAGE.get(), data.getDamageValues()[0]);
+            event.add(entityType, AttributeRegistry.WATER_DAMAGE.get(), data.getDamageValues()[1]);
+            event.add(entityType, AttributeRegistry.LIGHTNING_DAMAGE.get(), data.getDamageValues()[2]);
+            event.add(entityType, AttributeRegistry.MAGIC_DAMAGE.get(), data.getDamageValues()[3]);
+            event.add(entityType, AttributeRegistry.DARK_DAMAGE.get(), data.getDamageValues()[4]);
             //resistances
-            event.add(entityType, AttributeRegistry.FIRE_RESISTANCE.get());
-            event.add(entityType, AttributeRegistry.WATER_RESISTANCE.get());
-            event.add(entityType, AttributeRegistry.LIGHTNING_RESISTANCE.get());
-            event.add(entityType, AttributeRegistry.MAGIC_RESISTANCE.get());
-            event.add(entityType, AttributeRegistry.DARK_RESISTANCE.get());
+            event.add(entityType, AttributeRegistry.FIRE_RESISTANCE.get(), data.getResistanceValues()[0]);
+            event.add(entityType, AttributeRegistry.WATER_RESISTANCE.get(), data.getResistanceValues()[1]);
+            event.add(entityType, AttributeRegistry.LIGHTNING_RESISTANCE.get(), data.getResistanceValues()[2]);
+            event.add(entityType, AttributeRegistry.MAGIC_RESISTANCE.get(), data.getResistanceValues()[3]);
+            event.add(entityType, AttributeRegistry.DARK_RESISTANCE.get(), data.getResistanceValues()[4]);
         }
     }
 
