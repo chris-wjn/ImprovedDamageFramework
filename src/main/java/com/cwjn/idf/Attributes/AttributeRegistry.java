808f2a6d56e48423d80e56bf935b3bc2c0722699
Traboul <bungy1@hotmail.ca>
2022-04-09 21:58:34 -0700

    Added 6 attributes

--- src/main/java/com/cwjn/idf/Attributes/AttributeRegistry.java
+++ src/main/java/com/cwjn/idf/Attributes/AttributeRegistry.java
@@ -19,11 +19,21 @@ public class AttributeRegistry {
     public static final RegistryObject<Attribute> MAGIC_DAMAGE = register("idf.magic_damage", () -> new RangedAttribute("idf.attribute.magic_damage", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
     public static final RegistryObject<Attribute> DARK_DAMAGE = register("idf.dark_damage", () -> new RangedAttribute("idf.attribute.dark_damage", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
 
+    public static final RegistryObject<Attribute> LIFE_STEAL = register("idf.life_steal", () -> new RangedAttribute("idf.attribute.life_steal", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
+    public static final RegistryObject<Attribute> SLOWING = register("idf.slowing", () -> new RangedAttribute("idf.attribute.slowing", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
+    public static final RegistryObject<Attribute> PENETRATING = register("idf.penetrating", () -> new RangedAttribute("idf.attribute.penetrating", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
+
+
     public static final RegistryObject<Attribute> FIRE_RESISTANCE = register("idf.fire_resistance", () -> new RangedAttribute("idf.attribute.fire_resistance", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
     public static final RegistryObject<Attribute> WATER_RESISTANCE = register("idf.water_resistance", () -> new RangedAttribute("idf.attribute.water_resistance", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
     public static final RegistryObject<Attribute> LIGHTNING_RESISTANCE = register("idf.lightning_resistance", () -> new RangedAttribute("idf.attribute.lightning_resistance", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
     public static final RegistryObject<Attribute> MAGIC_RESISTANCE = register("idf.magic_resistance", () -> new RangedAttribute("idf.attribute.magic_resistance", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
     public static final RegistryObject<Attribute> DARK_RESISTANCE = register("idf.dark_resistance", () -> new RangedAttribute("idf.attribute.dark_resistance", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
+
+    public static final RegistryObject<Attribute> REGENERATING = register("idf.regenerating", () -> new RangedAttribute("idf.attribute.regenerating", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
+    public static final RegistryObject<Attribute> EVADING = register("idf.evading", () -> new RangedAttribute("idf.attribute.evading", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
+    public static final RegistryObject<Attribute> ADAPTING = register("idf.adapting", () -> new RangedAttribute("idf.attribute.adapting", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
+    // ^ basically Warframe sentients
 
     private static <T extends Attribute> RegistryObject<T> register(final String name, final Supplier<T> attribute) {
         return ATTRIBUTES.register(name, attribute);
