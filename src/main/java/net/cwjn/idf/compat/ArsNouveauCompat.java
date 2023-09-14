804ccf2e5e900084c8483ed6062535d3bd6de7ee
chris <chrisnelson315@gmail.com>
2023-09-14 14:56:00 -0700

    removed ars nouveau

--- src/main/java/net/cwjn/idf/compat/ArsNouveauCompat.java
+++ src/main/java/net/cwjn/idf/compat/ArsNouveauCompat.java
@@ -1,8 +1,13 @@
 package net.cwjn.idf.compat;
 
 import com.hollingsworth.arsnouveau.api.event.SpellDamageEvent;
+import com.hollingsworth.arsnouveau.api.spell.AbstractCastMethod;
 import com.hollingsworth.arsnouveau.api.spell.SpellContext;
 import com.hollingsworth.arsnouveau.api.spell.SpellSchools;
+import com.hollingsworth.arsnouveau.common.spell.method.MethodOrbit;
+import com.hollingsworth.arsnouveau.common.spell.method.MethodProjectile;
+import com.hollingsworth.arsnouveau.common.spell.method.MethodTouch;
+import net.cwjn.idf.damage.IDFIndirectEntityDamageSource;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,19 +21,34 @@ public class ArsNouveauCompat {
     }
 
     @SubscribeEvent
-    private static void convertSpellDamage(SpellDamageEvent.Pre event) {
-        float amount = event.damage;
+    public static void convertSpellDamage(SpellDamageEvent.Pre event) {
+        DamageSource source = event.damageSource;
         SpellContext ctx = event.context;
-        AtomicInteger fire = new AtomicInteger();
-        AtomicInteger water = new AtomicInteger();
-        AtomicInteger air = new AtomicInteger();
-        AtomicInteger earth = new AtomicInteger();
+        int[] schools = {0, 0, 0, 0};
+        final String[][] damageClass = {{"none"}};
         ctx.getSpell().recipe.forEach(spellIngredient -> {
-            if (spellIngredient.spellSchools.contains(SpellSchools.ELEMENTAL_FIRE)) fire.getAndIncrement();
-            if (spellIngredient.spellSchools.contains(SpellSchools.ELEMENTAL_WATER)) water.getAndIncrement();
-            if (spellIngredient.spellSchools.contains(SpellSchools.ELEMENTAL_AIR)) air.getAndIncrement();
-            if (spellIngredient.spellSchools.contains(SpellSchools.ELEMENTAL_EARTH)) earth.getAndIncrement();
+            if (spellIngredient.spellSchools.contains(SpellSchools.ELEMENTAL_FIRE)) schools[0]++;
+            if (spellIngredient.spellSchools.contains(SpellSchools.ELEMENTAL_WATER)) schools[1]++;
+            if (spellIngredient.spellSchools.contains(SpellSchools.ELEMENTAL_AIR)) schools[2]++;
+            if (spellIngredient.spellSchools.contains(SpellSchools.ELEMENTAL_EARTH)) schools[3]++;
+            if (spellIngredient instanceof AbstractCastMethod castMethod) {
+                if (castMethod instanceof MethodTouch || castMethod instanceof MethodOrbit) damageClass[0][0] = "strike";
+                else if (castMethod instanceof MethodProjectile) damageClass[0][0] = "pierce";
+            }
         });
+        int highest = 0;
+        for (int i = 0; i < 4; ++i) {
+            if (schools[i] > schools[highest]) highest = i;
+        }
+        switch (highest) {
+            case 0: event.damageSource = new IDFIndirectEntityDamageSource(source.msgId, source.getDirectEntity(), event.caster,
+                    1, 0, 0, 0, 0, 0, 0, 0, damageClass[0][0]);
+            case 1: event.damageSource = new IDFIndirectEntityDamageSource(source.msgId, source.getDirectEntity(), event.caster,
+                    0, 1, 0, 0, 0, 0, 0, 0, damageClass[0][0]);
+            default:
+                event.damageSource = new IDFIndirectEntityDamageSource(source.msgId, source.getDirectEntity(), event.caster,
+                    0, 0, 0, 0, 0, 0, 0, 0, damageClass[0][0]);
+        }
     }
 
 }
