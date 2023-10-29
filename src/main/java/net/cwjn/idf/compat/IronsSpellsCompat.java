3b98aad7b4e573a5909b2ce1951a17150c4c82dd
chris <chrisnelson315@gmail.com>
2023-10-29 14:12:54 -0700

    added compat for irons_spells 2.0.2

--- src/main/java/net/cwjn/idf/compat/IronsSpellsCompat.java
+++ src/main/java/net/cwjn/idf/compat/IronsSpellsCompat.java
@@ -7,7 +7,6 @@ public class IronsSpellsCompat {
 
     public static void register() {
         DamageSources.HEARTSTOP = new IDFDamageSource("heartstop", "none").setTrue();
-        DamageSources.BLEED_DAMAGE = new IDFDamageSource("bleed_effect", 0, 0, 0, 0, 1, 0, 0, 0, "none").setIsConversion();
         DamageSources.CAULDRON = new IDFDamageSource("cauldron", 0, 0, 0, 0, 1, 0, 0, 0, "none").setIsConversion();
     }
 
